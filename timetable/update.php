6c4ed543f2344f6187a59f40b7d59cf36f209d18
fossbin <stevedominicfez@gmail.com>
2024-02-27 04:30:31 +0530

    Fixed manual substitution

--- timetable/update.php
+++ timetable/update.php
@@ -7,15 +7,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the selected option from the form
     $cellId=$_POST['timetableId']; 
     $facultyId=$_POST['faculty'];
-    $sql="select bid from tbl_timetable where id='$cellId'";
+    $sql="select bid from tbl_timetable where id=$cellId";
     $result=$db->query($sql);
-    while($bid=$result->fetch_assoc())
-    $sql1 = "update tbl_timetable set fid ='$facultyId' where id ='$cellId'";
-    $sql2 = "update tbl_timetable set subid=(select subid from tbl_allocation where fid='$facultyId' and bid='$bid' LIMIT 1) where id='$cellId'";
-    if (($db->query($sql1)==true) && ($db->query($sql2)==true)) {
-        header("Location: display.php");
-        exit;
-    } else {
-        echo "Update failed: " . $db->error;
+    if($result){
+        $row = $result->fetch_assoc();
+        $bid = $row['bid'];
+        $sql1 = "update tbl_timetable set fid =$facultyId where id =$cellId";
+        if ($db->query($sql1)==true) {
+            $sql2 ="update tbl_timetable set subid=(select subid from tbl_allocation where fid=$facultyId and bid=$bid LIMIT 1) where id=$cellId";
+            if($db->query($sql2)==true){
+                header("Location: display.php");
+                exit;
+            } else {
+            echo "Update failed2: " . $db->error;
+            }
+        } else {
+            echo "Update failed1: " . $db->error;
+        }
     }
 }
